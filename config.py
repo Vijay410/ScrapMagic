953a8a2e449443c22bb776a4e06a7df523935232
vijay.vithal <vijay.vithal@stellapps.com>
2024-04-14 11:04:13 +0530

    Rest API

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #This file will hold your configuration variables,database connection details. 
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'admin'
+MYSQL_PASSWORD = '###'
 MYSQL_DB = 'books'
